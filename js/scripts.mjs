5b82a7eec5293422a3536495e2ace0f775d5fe8a
Usarneme <jamestcoop@gmail.com>
2021-04-09 12:05:25 -0700

    adds functionality to allow selecting a single pizza size (disabling other sizes upon a selection).

--- js/scripts.mjs
+++ js/scripts.mjs
@@ -1,14 +1,29 @@
 import { PizzaOrder, Pizza } from "./PizzaOrder.js"
 
-console.log("loaded scripts.js")
-console.log(Pizza)
+let size
 
 $(document).ready(function() {
   console.log('Document Ready!')
 
   $(".btn").click(function() {
-    console.log("Button pressed")
-    const isPressed = $(this).attr("aria-pressed")
-    $(this).attr("aria-pressed", !isPressed)
+    console.log("Button pressed", $(this).attr("aria-pressed"))
+    // console.log($(this))
+    // if this button used to be active, the user has undone their size selection
+    if ($(this).attr("aria-pressed") === "true") {
+      $(".size-button").attr("disabled", false)
+      size = undefined
+      console.log("size reset",size)
+    } else {
+      // when a size selection is made, disable all other size-buttons
+      $(".size-button").attr("disabled", true)
+      // re-enable the button for the size that was selected
+      // allowing user to undo selection by clicking again
+      // while limiting user to selecting only a single size
+      $(this).attr("disabled", false)
+      // set the size of the pizza
+      // each button has a unique size class always the fourth class in the list of classes
+      size = $(this).attr("class").split(" ")[3].toUpperCase()
+      console.log("size set",size)
+    }
   })
 })
